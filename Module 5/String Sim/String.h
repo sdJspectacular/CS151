9d84a95b4d61c933d4c0d2730082080b20178b39
sdJspectacular <sd.jack.m@gmail.com>
2025-10-01 17:39:22 +0000

    hi

--- Module 5/String Sim/String.h
+++ Module 5/String Sim/String.h
@@ -10,26 +10,20 @@ private:
     int size;
 
 public:
-    String(const char *s = ""); // constructor
-    String(const String &rhs);       // copy constructor
-    ~String();                  // destructor
-    void change(const char *);  // reassign string
+    // constructor
+    String(const char *s = "");
+    String(const String &rhs); // copy constructor
+    ~String();                 // destructor
+    void change(const char *); // reassign string
     // if this friend function is commented out, the compiler throws this message?
     //   ‘print’ was not declared in this scope; did you mean ‘printf’?
     friend void print(const String &); // friend function
 };
 
-// 4. Below the  String  class (but still in the  String.h  file), the constructor for this
-// class should take a  C-string,  store in in the heap, and assign it to the  string
-// data member.  Notice that this constructor uses two functions from the C++ Standard
-// Library (strlen  and  strcpy).  Please include a comment above each function you enter
-// below, so I know what each one does:
-
-// Constructor
 String::String(const char *s)
 {
     size = strlen(s);
-    string = new char[size + 1];
+    string = new char[size + 1]; // add 1 for the null terminator
     strcpy(string, s);
 }
 
