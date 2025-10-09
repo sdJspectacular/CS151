f8dcd9a39841afdc183574f4ff8bbeb512cf8227
sdJspectacular <sd.jack.m@gmail.com>
2025-10-09 07:51:37 +0000

    test

---
+++ Module 7/test.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Person
+{
+private:
+    string id;
+
+protected:
+    string name;
+
+public:
+    Person() { setName(""); }
+    Person(const string &pName) { setName(pName); }
+    void setName(const string &pname) { name = pname; }
+    string getName() const { return name; }
+};
+
+class Student : public Person
+{
+    private:
+    string major;
+    public:
+    Student(const string &sname, const string &discp)
+    {
+        major = discp;
+        name = sname;
+        id = 0;
+    }
+
+};
+
+int main()
+{
+    Person p1("joe");
+    Student s1("joe", "cs");
+    s1.id = "blah";
+    s1.name = "blah";
+    s1.major = "blah";
+
+    p1.id = "blah";
+    p1.name = "blah";
+    
+    return 0;
+}
