39b9f9168afecf6b7a83d9e82b67aa2cec7e6a31
Jack Marriott <j_marriott@apple.com>
2025-10-09 00:53:53 -0700

    lab 7

---
+++ Module 7/PersonAtMCC.h
@@ -0,0 +1,80 @@
+/* PersonAtMCC.h - The parent class of Instructor, Student, and Staff
+ * Author:     <your name>
+ * Module:     7
+ * Project:    Lab, Part 1
+ * Description:
+ *
+ *    Data members:
+ *       id (string)
+ *       name (string)
+ *       address (string)
+ *       phone (string)
+ *
+ *    Functions:
+ *       no-argument constructor
+ *       partial constructor (id & name)
+ *       full constructor
+ *       copy constructor
+ *       all getters and setters
+ *       showinfo
+ */
+
+#pragma once
+
+#include <iostream>
+using namespace std ;
+
+class PersonAtMCC {
+
+    private :
+        long long id ;
+        string name ;
+        string address ;
+        string phone ;
+
+    public :
+
+        PersonAtMCC() {
+            PersonAtMCC(0, "no name yet", "no address yet", "no phone yet") ;
+        }
+
+        PersonAtMCC(long long id, string name) {
+            PersonAtMCC(id, name, "no address yet", "no phone yet") ;
+        }
+
+        // **** Complete these constructors using initialization lists **** /
+
+        // full constructor
+        PersonAtMCC(long long id, string name, string address, string phone) { }
+
+        // copy constructor
+        PersonAtMCC(const PersonAtMCC &copy) { }
+
+        // getters
+        long long getId() const
+            {  return id ; }
+        string getName() const
+            { return name ; }
+        string getAddress() const
+            { return address ; }
+        string getPhone() const
+            { return phone ; }
+
+        // setters
+        void setId(long long id)
+            { this->id = id ; }
+        void setName(string name)
+            { this->name = name ; }
+        void setAddress(string address)
+            { this->address = address ; }
+        void setPhone(string phone)
+            { this->phone = phone ; }
+
+        // show person's info on the screen
+        void showInfo() {
+            cout << "ID: " << id
+                 << "  Name: " << name
+                 << "  Address: " << address << endl
+                 << "    Phone: " << phone << endl ;
+        }
+} ;
---
+++ Module 7/Staff.h
@@ -0,0 +1,69 @@
+/* Staff.h - Header for staff members at MCC
+ * Author:     <your name>
+ * Module:     7
+ * Project:    Lab
+ * Description:
+ *
+ *    Child of PersonAtMCC
+ *
+ *    Data members:
+ *       extension (string)
+ *       hire_date (string)
+ *       pay_rate (double)
+ *
+ *    Functions:
+ *       full constructor
+ *       copy constructor
+ *       getters and setters for extension, hire_date, and pay_rate
+ *       showInfo
+ */
+
+#pragma once
+
+#include <iostream>
+using namespace std ;
+
+         /**** Make this class a child of PersonAtMCC with public access ****/
+
+class Staff {
+
+    private :
+        string extension ;
+        string hire_date ;
+        double pay_rate ;
+
+    public :
+
+        /**** Complete these constructors using initialization lists ****/
+
+        // full constructor
+        Staff(long long id, string address, string name, string phone,
+                   string extension, string hire_date, double pay_rate) { }
+
+        // copy constructor
+        Staff(const Staff &copy) { }
+
+        // getters
+        string getExtension() const
+            { return extension ; }
+        string getHireDate() const
+            { return hire_date ; }
+        double getPayRate() const
+            { return pay_rate ; }
+
+        // setters
+        void setExtension(string extension)
+            { this->extension = extension ; }
+        void setHireDate(string hire_date)
+            { this->hire_date = hire_date ; }
+        void setPayRate(double pay_rate)
+            { this->pay_rate = pay_rate ; }
+
+        /**** Complete this function ****/
+
+        // Display the staff member's info on the screen
+        void showInfo() {
+             cout << "ID: " << getId() << endl ;
+        }
+} ;
+
