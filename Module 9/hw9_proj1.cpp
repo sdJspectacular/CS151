81b8d4da2a8ea952092d4a4f4e75609438ad2fa5
sdJspectacular <sd.jack.m@gmail.com>
2025-11-08 07:00:16 +0000

    hi

--- Module 9/hw9_proj1.cpp
+++ Module 9/hw9_proj1.cpp
@@ -18,19 +18,47 @@ int main()
 {
     string inFile, outFile;
     
+    // Asks the user for two filenames.  The first file should be opened for input,
+    // and the second file should be opened for output. 
     cout << "Enter the input file name: ";
     getline(cin, inFile);
     if (inFile == "");
         inFile = "A Study In Scarlet.txt";
     cout << "Your input file: " << inFile << "\n";
     
+    ifstream inDataFile(inFile);
+    if (!inDataFile.is_open())
+    {
+        cout << "ERROR: could not open input file " << inFile << "\n";
+        exit(1);
+    }
+
     cout << "Enter the output file name: ";
     getline(cin, outFile);
     if (outFile == "");
         outFile = "A_Study_In_Scarlet.txt";
     cout << "Your output file: " << outFile << "\n";
     
-    
+    ofstream outDataFile(inFile);
+    if (!outDataFile.is_open())
+    {
+        cout << "ERROR: could not open output file " << outFile << "\n";
+        exit(1);
+    }
+
+    // The program will read the contents of the first file character-by-character
+    // and convert every character to lowercase with exceptions.
+    char ch;
+    while (inDataFile.get(ch))
+    {
+        cout << ch << "\n";
+    }
+
+    cout << "\n";
+
+    inDataFile.close();
+    outDataFile.close();
+
     return 0;
 }
 
