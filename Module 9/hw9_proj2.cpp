66afc566d00a59daf44d2e9358e484dbcec9a514
sdJspectacular <sd.jack.m@gmail.com>
2025-11-09 00:44:22 +0000

    yeah

--- Module 9/hw9_proj2.cpp
+++ Module 9/hw9_proj2.cpp
@@ -5,23 +5,38 @@
  * Problem statement:  Handle inventory data in a binary file
  *
  * Algorithm:
- *   1. 
+ *   1.
  */
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <cstring>
 
 using namespace std;
 
 const int NCHARS = 80;
+
+// A structure to store inventory information
 struct item_t
 {
-    char description[NCHARS];
+    char description[NCHARS]; // use C-string
     int qty;
     double cost, price;
-    char dateAdded[NCHARS];
+    char dateAdded[NCHARS]; // use C-string
 
+    // Use a constructor to avoid string inputs that exceed NCHARS
+    item_t(const char *tdescr, const int tqty, const double tcost, const double tprice, const char *tdate) : qty(tqty), cost(tcost), price(tprice)
+    {
+        strncpy(description, tdescr, NCHARS - 1);
+        description[NCHARS] = '\0';
+
+        strncpy(dateAdded, tdate, NCHARS - 1);
+        dateAdded[NCHARS] = '\0';
+    }
+
+    // Also need a default constructor
+    item_t() = default;
 };
 
 int main()
@@ -29,20 +44,21 @@ int main()
     string fname = "inventory.dat";
 
     vector<item_t> items;
-    items.push_back({"Item A", 10, 10.50, 14.99, "25-Dec-2024"});
-    items.push_back({"Item B", 100, 0.99, 2.99, "25-Dec-2024"});
-    items.push_back({"Item C", 50, 4.09, 7.67, "25-Dec-2024"});
 
-    ofstream data_file(fname, ios::binary | ios::app);
-    if (!data_file)
-    {
-        cout << "ERROR: could not open " << fname << "\n";
-        exit(1);
-    }
+    // If the inventory file doesn't exist, create one when first running
+    // your program
 
-    for (auto it : items)
+    fstream data_file(fname, ios::binary | ios::in | ios::out);
+
+    if (!data_file.good())
     {
-        data_file.write(reinterpret_cast<const char*>(&it), sizeof(item_t));
+        items.push_back(item_t{"Item A", 10, 10.50, 14.99, "25-Dec-2024"});
+        items.push_back(item_t{"Item B", 100, 0.99, 2.99, "25-Dec-2024"});
+        items.push_back(item_t{"Item C", 50, 4.09, 7.67, "25-Dec-2024"});
+        for (const auto &it : items)
+        {
+            data_file.write(reinterpret_cast<const char *>(&it), sizeof(item_t));
+        }
     }
 
     data_file.close();
