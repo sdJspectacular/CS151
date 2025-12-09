8bae42abb6f756e935b1604c9b5df288fd988e40
sdJspectacular <sd.jack.m@gmail.com>
2025-12-09 02:03:41 +0000

    hw14

--- HW14/LinkedList.h
+++ HW14/LinkedList.h
@@ -1,16 +1,24 @@
+/*
+   1. Project 1: design a linked list class with two member functions: add and isMember
+      - The add function adds a new node containing a double value to the head of the list
+      - The isMember function checks whether a given double value is present in the list
+
+
+*/
 #include <iostream>
 
 using namespace std;
 
-template <typename T>
+// Definition of the LinkedList class based on Gaddis book, page 1149
 class LinkedList
 {
 private:
     struct ListNode
     {
-        T value;
+        double value;
         ListNode *next;
-        ListNode(T value1, ListNode *next1 = nullptr)
+        // Constructor
+        ListNode(double value1, ListNode *next1 = nullptr)
         {
             value = value1;
             next = next1;
@@ -23,7 +31,53 @@ public:
         head = nullptr;
     }
     ~LinkedList();
-    void add(T value);
-    void remove(T value);
-    void displayList() const;
+    void add(double value);
+    bool isMember(double number) const;
 };
+
+LinkedList::~LinkedList()
+{
+    ListNode *current = head;
+    while (current != nullptr)
+    {
+        ListNode *nextNode = current->next;
+        delete current;
+        current = nextNode;
+    }
+}
+
+// The add function adds a new node containing <value> to the head of the list
+void LinkedList::add(double value)
+{
+    // Create the new node
+    ListNode *newNode = new ListNode(value);
+
+    if (head == nullptr)
+    {
+        // Add to head if list is empty
+        head = newNode;
+    }
+    else
+    {
+        // Link the new node to the current head
+        newNode->next = head;
+        head = newNode;
+    }
+}
+
+bool LinkedList::isMember(double number) const
+{
+    ListNode *current = head;
+
+    // Walk the list
+    while (current != nullptr)
+    {
+        if (current->value == number)
+        {
+            return true;  // found it
+        }
+        current = current->next;
+    }
+
+    return false;  // did not find it
+}
---
+++ HW14/hw14.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "This is a placeholder for HW14 main function.\n";
+    return 0;
+}
